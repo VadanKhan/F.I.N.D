8204478e6ef5946a569d1a5164b7356a988b4386
Will Beresford <Will.Beresford@YASA.com>
2023-06-20 16:57:16 +0100

    pipeline fixes

--- eolt_root_cause_analyser/__main__.py
+++
@@ -1,7 +0,0 @@
-"""Command-line interface."""
-def main() -> None:
-    """Eolt Root Cause Analyser."""
-
-
-if __name__ == "__main__":
-    main(prog_name="eolt-root-cause-analyser")  # pragma: no cover
