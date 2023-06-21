f18faa83dd08f2d56f414a9989de68a0694a1869
Will Beresford <Will.Beresford@YASA.com>
2023-06-21 09:01:40 +0100

    #255168 added tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,13 @@
 """Test cases for the __main__ module."""
-import pytest
+from unittest import mock
+
+import click.testing
+import eolt_root_cause_analyser.cli as cli
 
 
-def dummy_test():
-    assert True
+def test_begin_prints_error_too_many():
+    """This tests that an error message is printed when there are too many args."""
+    runner = click.testing.CliRunner()
+    with mock.patch("builtins.print") as print:
+        runner.invoke(cli.begin, ["--info", "string1, string2, string3, string4"])
+        print.assert_called_once_with("Error: Too many arguments")
