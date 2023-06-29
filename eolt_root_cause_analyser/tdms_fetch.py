6dcce644fdf53746cdfde80217a1c569b8d8cf2f
OYM\Vadan.Khan <vadan.khan@yasa.com>
2023-06-29 10:46:46 +0100

    Working QNAP fetch

--- eolt_root_cause_analyser/tdms_fetch.py
+++ eolt_root_cause_analyser/tdms_fetch.py
@@ -4,10 +4,19 @@ import pandas as pd
 import yasa_file_io.tdms
 
 
-def form_filename(test_id, test_type_id, eol_test_id):
-    filename = f"{eol_test_id}_{test_type_id}_{test_id}"
+def form_filename_tdms(test_id, test_type_id, eol_test_id):
+    filename = f"{eol_test_id}_{test_type_id}_{test_id}.tdms"
     return filename
 
 
-ans = form_filename(20140, "High_Speed", 23918)
-print(ans)
+def read_tdms(filename):
+    df = yasa_file_io.tdms.read_tdms_as_dataframe(
+        Path(rf"\\QNAP-463\eol\{filename}"),
+        channel_map={"Ambient_new": "Ambient"},
+        extract_all=True,
+        fuzzy_matching=True,
+        drop_duplicates=False,
+        fuzzy_match_score=50.0,
+        search_terms_as_keys=False,
+    )
+    return df
