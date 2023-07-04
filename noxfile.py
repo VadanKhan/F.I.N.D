e7c984c245f6956923d72e74285e57c81fa7cac3
Vadan Khan <vadan.khan@yasa.com>
2023-07-04 14:58:26 +0000

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -11,7 +11,7 @@ from typing import Iterator
 import nox
 
 package = "eolt_root_cause_analyser"
-python_versions = ["3.10", "3.9"]
+python_versions = ["3.10"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
