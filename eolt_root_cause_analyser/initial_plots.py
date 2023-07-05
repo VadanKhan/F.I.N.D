8e50c0fee3e4fe05fd020f2b0d2fff5d9de0f8ad
OYM\Vadan.Khan <vadan.khan@yasa.com>
2023-07-05 11:04:51 +0100

    #256124 Check tdms_fetch

--- eolt_root_cause_analyser/initial_plots.py
+++ eolt_root_cause_analyser/initial_plots.py
@@ -1,34 +1,48 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from tdms_fetch import form_filepath
+from tdms_fetch import get_time_series_data
 from tdms_fetch import read_tdms
 
-df_test = read_tdms("23918_High_Speed_20140.tdms")
+df_filepath = form_filepath("23918_High_Speed_20140.tdms")
+df_test = read_tdms(df_filepath)
+RPS_time_list = get_time_series_data(df_filepath, ["AI DAQ - High Speed Inc RTD"], ["SinP", "SinN", "CosP", "CosN"])
+SinP = RPS_time_list[0]
+print(SinP)
+# time_dfs = get_time_series_data()
 
 
+# %%
 def initial_plots(df):
-    df = df.reset_index()
-    # index_subset = df[(df['index'] >= 650) & (df['index'] <= 650.1 )]
-    index_subset2 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
-    index_subset3 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
-    index_subset4 = df[(df["index"] >= 200000) & (df["index"] <= 201000)]
-    index_subset5 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
-    # df.plot("index", y=["U", "V", "W"])
-    index_subset2.plot("index", y=["U", "V", "W"])
-    df.plot("index", y=["Speed_original"])
-    df.plot("index", y=["Torque"])
-    # index_subset4.plot("index", y=["Microphone"])
+    # df = df.reset_index()
+    # # index_subset = df[(df['index'] >= 650) & (df['index'] <= 650.1 )]
+    # index_subset2 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
+    # index_subset3 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
+    # index_subset4 = df[(df["index"] >= 200000) & (df["index"] <= 201000)]
+    # index_subset5 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
+    # # df.plot("index", y=["U", "V", "W"])
+    # index_subset2.plot("index", y=["U", "V", "W"])
+    # df.plot("index", y=["Speed_original"])
+    # df.plot("index", y=["Torque"])
+    # # index_subset4.plot("index", y=["Microphone"])
     plt.show()
 
 
+# %%
 df = df_test
-df = df.reset_index()
+# df = df.reset_index()
+print(f"{df}\n")
 columns = list(df.columns)
 print(columns)
+
+
+# sinN = df["AI DAQ - High Speed Inc RTD/SinN"]
+# print(sinN)
 # index_subset = df[(df['index'] >= 650) & (df['index'] <= 650.1 )]
-index_subset2 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
-index_subset3 = df[(df["index"] >= 200000) & (df["index"] <= 201000)]
-index_subset4 = df[(df["index"] >= 266000) & (df["index"] <= 285000)]
-index_subset5 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
+# index_subset2 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
+# index_subset3 = df[(df["index"] >= 200000) & (df["index"] <= 201000)]
+# index_subset4 = df[(df["index"] >= 266000) & (df["index"] <= 285000)]
+# index_subset5 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
 # df.plot("index", y=["U", "V", "W"])
 
 # index_subset2.plot("index", y=["U", "V", "W"])
@@ -36,6 +50,6 @@ index_subset5 = df[(df["index"] >= 200000) & (df["index"] <= 200100)]
 # df.plot("index", y=["Torque"])
 # index_subset4.plot("index", y=["Microphone"])
 # df.plot("index", y=["Rotor_1_original"])
-index_subset4.plot("index", y=["SinP", "SinN", "CosP", "CosN"])
-index_subset4.plot("index", y=["U", "V", "W"])
+# index_subset4.plot("index", y=["SinP", "SinN", "CosP", "CosN"])
+# index_subset4.plot("index", y=["U", "V", "W"])
 plt.show()
--- eolt_root_cause_analyser/tdms_fetch.py
+++ eolt_root_cause_analyser/tdms_fetch.py
@@ -1,9 +1,95 @@
-import warnings
+import re
+import sys
 from pathlib import Path
+from typing import Any
+from typing import Optional
+from warnings import warn
 
 import pandas as pd
-import yasa_file_io.tdms
+import thefuzz.process as fuzz
 from nptdms import TdmsFile
+
+
+def _fuzzy_match_dictionary(tdms_columns: list, channel_map: dict, fuzzy_match_score: float) -> dict:  # type: ignore
+    """Creates a new dictionary that fuzzy matches the key to the column.
+
+    Args:
+        tdms_columns (list): all the columns in the dataframe
+        channel_map (dict): dictionary containing the channel map
+        fuzzy_match_score (float): threshold the fuzzy score should be above
+
+    Returns:
+        new_channel_map (dict): new channel map, key=fuzzy matched column, val=name to change col to
+    """
+    new_channel_map = {}
+    for key, val in channel_map.items():
+        key_match, fuzzy_score = fuzz.extractOne(key, tdms_columns)
+        new_channel_map[key_match] = val
+        if fuzzy_score <= fuzzy_match_score:
+            warn(
+                f"fuzzy matched column scored {fuzzy_score}%, below threshold of {fuzzy_match_score}%. {key} mapped to {key_match}",
+                stacklevel=2,
+            )
+
+    return new_channel_map
+
+
+def tdms_mapping_warning(tdms_columns, channel_map):
+    """Prints out warnings if a channel key does not match a column exactly.
+
+    Args:
+        tdms_columns (list): all the columns in the dataframe
+        channel_map (dict): dictionary of channel maps
+
+    """
+    for key, val in channel_map.items():
+        if key in tdms_columns:
+            continue
+        else:
+            warn(f"channel key, {key}, did not match a column in the dataframe. {tdms_columns}")
+
+
+def _apply_channel_map(
+    tdms_file: pd.DataFrame,
+    channel_map: dict,
+    fuzzy_matching: bool,
+    drop_duplicates: bool,
+    extract_all: Optional[bool] = False,
+    fuzzy_match_score: Optional[float] = 60.0,
+) -> pd.DataFrame:
+    """Applies the channel map to the dataframe.
+
+    Args:
+        tdms_file (pd.DataFrame): tdms file as a dataframe
+        channel_map (dict): dictionary containing the channel map
+        fuzzy_matching (bool): whether to use fuzzy matching
+        drop_duplicates (bool): whether to drop duplicates
+        extract_all (bool, optional): whether to extract all channels. Defaults to False.
+        fuzzy_match_score (float, optional): fuzzy score threshold, if fuzzy_match=True
+
+    Returns:
+        pd.DataFrame: tdms file with the columns renamed according to the channel map
+    """
+    # remove the groups from the columns names for convenience. Use regex to find the text between the delimiting chars
+    tdms_file.columns = [re.search(r"'\/'(.*?)'", column).group(1) for column in tdms_file.columns]  # type: ignore
+
+    # new channel_map - keys were fuzzy matched to a column name and a new dictionary was generated
+    if fuzzy_matching:
+        channel_map = _fuzzy_match_dictionary(tdms_file.columns, channel_map, fuzzy_match_score)  # type: ignore
+
+    tdms_mapping_warning(tdms_file.columns, channel_map)  # writes warnings if dictionary keys dont match column name
+
+    if extract_all:
+        renamed_tdms = tdms_file.rename(columns=channel_map, inplace=False)
+        tdms_file = tdms_file.join(renamed_tdms, how="outer", lsuffix="_original", sort=False)
+
+    else:
+        tdms_file.rename(columns=channel_map, inplace=True)
+        tdms_file.drop(labels=tdms_file.columns.difference(channel_map.values()), axis=1, inplace=True)  # type: ignore
+
+    if drop_duplicates:
+        tdms_file = tdms_file.loc[:, ~tdms_file.columns.duplicated()].copy()
+    return tdms_file
 
 
 def form_filename_tdms(test_id, test_type_id, eol_test_id):
@@ -22,46 +108,83 @@ def form_filename_tdms(test_id, test_type_id, eol_test_id):
     return filename
 
 
-def read_tdms(filename):
-    """Reads a EOL TDMS file and returns its contents as a Pandas DataFrame.
-    NOTE: THIS STEP HAS A LARGE RUNTIME ~20 SECONDS if connecting to QNAP
-
-    Args:
-        filename (str): The name of the TDMS file to read.
-
-    Returns:
-        DataFrame: A Pandas DataFrame containing the data from the TDMS file.
-    """
-    try:
-        df = yasa_file_io.tdms.read_tdms_as_dataframe(
-            Path(rf"C:\Users\Vadan.Khan\Documents\Project\Sample TDMS files\{filename}"),
-            channel_map={"Ambient_new": "Ambient"},
-            extract_all=True,
-            fuzzy_matching=True,
-            drop_duplicates=False,
-            fuzzy_match_score=50.0,
-            search_terms_as_keys=False,
-        )
-        return df
-    except Exception:
-        warnings.warn("Accessing TDMS Failed", UserWarning)
-        return None
-
-
-def read_tdms_to_df(filename):
+def form_filepath(filename):
     tdms_file_path = Path(rf"C:\Users\Vadan.Khan\Documents\Project\Sample TDMS files\{filename}")
+    return tdms_file_path
+
+
+def read_tdms(tdms_file_path):
     tdms_file = TdmsFile.read(tdms_file_path)
 
     # Converting TDMS data to a pandas DataFrame
-    data = {}
-    for group in tdms_file.groups():
-        group_name = group.name
-        for channel in group.channels():
-            channel_name = channel.name
-            column_name = f"{group_name}/{channel_name}"
-            data[column_name] = channel[:]
-
+    df = tdms_file.as_dataframe()
+    tdms_df = _apply_channel_map(
+        df,
+        channel_map={"Ambient_new": "Ambient"},
+        fuzzy_matching=True,
+        drop_duplicates=True,
+        extract_all=True,
+        fuzzy_match_score=60,
+    )
     # Creating a DataFrame with columns of unequal length
-    df = pd.DataFrame({k: pd.Series(v) for k, v in data.items()})
+    # df = pd.DataFrame({k: pd.Series(v) for k, v in data.items()})
+    return tdms_df
 
-    return df
+
+def get_time_series_data(tdms_filepath, group_names: list[Any], channel_names: list[Any]) -> list[pd.DataFrame]:
+    """
+    Gets the desired raw data (all dataframes stored in a list) along with test details.
+
+    Args:
+        test_type (str): Defines the test type (either Cogging, High Speed or High Torque)
+        test_id (int): Test ID for given test type
+        group_names (list): The group names which the desired parameter is part of in TDMS file
+        channel_names (list): List of channel names of desired parameter
+
+    Returns:
+        tdms_data (list): Containing DataFrames, each being the channel data
+        test_info (DataFrame): Contains basic info including Test ID's, times and SN
+
+    """
+    tdms_data = []
+    try:
+        with TdmsFile.open(tdms_filepath) as tdms_file:
+            for count, group in enumerate(group_names):
+                group = tdms_file[group]  # Specifies the group to look at
+                channel_name = channel_names[count]
+                channel = group[channel_name]  # Specifies the channel to look at
+                time_data = channel.time_track()  # Gets the time values for the channel
+                channel_data = channel[:]  # Gets raw data from the channel into an array
+                d = {(channel_name + "_Time"): time_data, channel_name: channel_data}
+                df = pd.DataFrame(data=d)
+                tdms_data.append(df)
+    except:
+        print("WARNING: check QNAP connection")
+        print(sys.exc_info())
+    return tdms_data
+
+
+# def read_tdms(filename):
+#     """Reads a EOL TDMS file and returns its contents as a Pandas DataFrame.
+#     NOTE: THIS STEP HAS A LARGE RUNTIME ~20 SECONDS if connecting to QNAP
+
+#     Args:
+#         filename (str): The name of the TDMS file to read.
+
+#     Returns:
+#         DataFrame: A Pandas DataFrame containing the data from the TDMS file.
+#     """
+#     try:
+#         df = yasa_file_io.tdms.read_tdms_as_dataframe(
+#             Path(rf"C:\Users\Vadan.Khan\Documents\Project\Sample TDMS files\{filename}"),
+#             channel_map={},
+#             extract_all=True,
+#             fuzzy_matching=True,
+#             drop_duplicates=False,
+#             fuzzy_match_score=50.0,
+#             search_terms_as_keys=False,
+#         )
+#         return df
+#     except Exception:
+#         warnings.warn("Accessing TDMS Failed", UserWarning)
+#         return None
